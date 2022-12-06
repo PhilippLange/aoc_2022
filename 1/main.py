7a32b221439a366302070216d6bf1645b63572c3
Philipp Lange <philipp.lange@mhp.com>
2022-12-06 15:26:29 +0100

    day 1

---
+++ 1/main.py
@@ -0,0 +1,8 @@
+file = './1/input.txt'
+with open(file) as fn:
+    raw = fn.read()
+
+parsed = [ [ int(c) for c in e.split('\n') ] for e in raw.split('\n\n') ]
+sum_elf = [ sum(c) for c in parsed ]
+sum_elf.sort(reverse=True)
+print(sum_elf[0])
