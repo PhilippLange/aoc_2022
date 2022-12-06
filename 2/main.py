5df5355e11269192af7251cc0811457531130314
Philipp Lange <philipp.lange@mhp.com>
2022-12-06 16:09:26 +0100

    day 2-2

--- 2/main.py
+++ 2/main.py
@@ -3,12 +3,12 @@ with open(file) as fn:
     raw = fn.read()
 
 t = {
-    "X": "r",
     "A": "r",
-    "Y": "p",
+    "X": "r",
     "B": "p",
-    "Z": "s",
+    "Y": "p",
     "C": "s",
+    "Z": "s",
 }
 
 p = {
@@ -18,18 +18,32 @@ p = {
 }
 
 w = {
-    ("r", "p"),
-    ("p", "s"),
-    ("s", "r"),
+    "r": "p",
+    "p": "s",
+    "s": "r",
 }
+
+l = {v: k for k, v in w.items()}
 
 def score(a):
     r = p[a[1]]
     if a[0] == a[1]:
         return r + 3
-    if (a[0], a[1]) in w:
+    if w[a[0]] ==  a[1]:
         return r + 6
     return r
 
+def transform(a):
+    a[0] = t[a[0]]
+    if a[1] == 'Y':
+        a[1] = a[0]
+    if a[1] == 'Z':
+        a[1] = w[a[0]]
+    if a[1] == 'X':
+        a[1] = l[a[0]]
+    return a
+
 parsed = [ score([ t[i] for i in ln.split(" ") ]) for ln in raw.splitlines() ]
+print(sum(parsed))
+parsed = [ score(transform(ln.split(" "))) for ln in raw.splitlines() ]
 print(sum(parsed))
